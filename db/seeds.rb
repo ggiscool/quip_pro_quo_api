625e4dd8892933da5039fe2b266356457a6f9c97
Sean Byrne <spickles@Seans-iMac.local>
2018-01-11 18:20:17 -0500

    Initial data seeded, and awesome_print gem installed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,87 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Category.create([
+  {name: "Movies"},
+  {name: "Sports"},
+  {name: "Food"}
+])
+
+Question.create([
+  {
+    title: "Disney Movie",
+    content: "The next rated R Disney Movie will be called ____",
+    category_id: 1
+  },
+  {
+    title: "Batman",
+    content: "The main villin in the next Batman movie is ____",
+    category_id: 1
+  },
+  {
+    title: "Titanic",
+    content: "Why did the titanic really sink",
+    category_id: 1
+  },
+  {
+    title: "James Bond",
+    content: "A device James Bond would use to excape danger",
+    category_id: 1
+  },
+  {
+    title: "The Notebook 2",
+    content: "The Notebook 2 starring ____ and ____",
+    category_id: 1
+  },
+  {
+    title: "First Sport Name",
+    content: "The first sport name to ever get rejected was",
+    category_id: 2
+  },
+  {
+    title: "Basketball",
+    content: "A basketball should really have been called this",
+    category_id: 2
+  },
+  {
+    title: "Best Team Name",
+    content: "The best possible name for a team is",
+    category_id: 2
+  },
+  {
+    title: "Tennis",
+    content: "The original name of tennis was",
+    category_id: 2
+  },
+  {
+    title: "New Sport",
+    content: "The name of a sport that needs to be made is",
+    category_id: 2
+  },
+  {
+    title: "Worst Condiment",
+    content: "The worst flavor for a condiment is ____",
+    category_id: 3
+  },
+  {
+    title: "Hot Dogs",
+    content: "The secret ingredient in hot dogs is ____",
+    category_id: 3
+  },
+  {
+    title: "Cheesy Name",
+    content: "If you had to rename cheese, what would you name it?",
+    category_id: 3
+  },
+  {
+    title: "Food Craze",
+    content: "The newest craze in fast food is ____",
+    category_id: 3
+  },
+  {
+    title: "Wine",
+    content: "If you had to upgrade the name 'wine', what would you call it?",
+    category_id: 3
+  },
+])
