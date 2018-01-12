57a00c27cff00b71344314c61a681f52fda4691e
Sean Byrne <spickles@Seans-iMac.local>
2018-01-12 11:41:28 -0500

    Create Route works

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -1,5 +1,5 @@
 class AnswersController < ApplicationController
-  before_action :set_answer, only: [:show, :update, :destroy]
+  # before_action :set_answer, only: [:show, :update, :destroy]
 
   # GET /answers
   def index
@@ -8,34 +8,16 @@ class AnswersController < ApplicationController
     render json: @answers
   end
 
-  # GET /answers/1
-  def show
-    render json: @answer
-  end
-
   # POST /answers
   def create
-    @answer = Answer.new(answer_params)
+    answer = Answer.new(answer_params)
+    answer.question_id = params[:question_id]
 
-    if @answer.save
-      render json: @answer, status: :created, location: @answer
+    if answer.save
+      render json: answer, status: :created
     else
-      render json: @answer.errors, status: :unprocessable_entity
+      render json: answer.errors, status: :unprocessable_entity
     end
-  end
-
-  # PATCH/PUT /answers/1
-  def update
-    if @answer.update(answer_params)
-      render json: @answer
-    else
-      render json: @answer.errors, status: :unprocessable_entity
-    end
-  end
-
-  # DELETE /answers/1
-  def destroy
-    @answer.destroy
   end
 
   private
