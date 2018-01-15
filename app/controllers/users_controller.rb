c577ee764d7d40cc94118442bf81215f151173f2
Sean Byrne <spickles@Seans-iMac.local>
2018-01-15 11:24:58 -0500

    added token to create user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -17,12 +17,14 @@ class UsersController < ApplicationController
 
   # POST /users
   def create
-    @user = User.new(user_params)
+    user = User.new(user_params)
 
-    if @user.save
-      render json: @user, status: :created, location: @user
+    if user.save
+      # render json: user, status: :created, location: user
+      token = create_token(user.id, user.username)
+      render json: {status: :created, location: user, token: token, user: user}
     else
-      render json: @user.errors, status: :unprocessable_entity
+      render json: user.errors, status: :unprocessable_entity
     end
   end
 
